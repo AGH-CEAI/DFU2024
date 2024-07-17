6357f725780e882369d0ab6fcbf6a508a817482b
Dariusz <darekk@agh.edu.pl>
2024-07-17 23:12:05 +0200

    dodanie hasla i inne poprawki

--- eval_online.py
+++ eval_online.py
@@ -5,6 +5,7 @@ import re
 import shutil
 import tempfile
 from datetime import datetime
+from hashlib import sha256
 from zipfile import ZipFile
 
 from flask import Flask, request
@@ -48,25 +49,25 @@ def main_page():
 
         only_testset_path = os.path.join(eval_online_dir, dir, 'only_testset.txt')
         if os.path.exists(only_testset_path):
-            test_set_tests.append([avg_dice, comment])
+            test_set_tests.append([avg_dice, comment, dir])
         else:
-            whole_db_tests.append([avg_dice, comment])
+            whole_db_tests.append([avg_dice, comment, dir])
 
     test_set_tests = list(sorted(test_set_tests, key=lambda x: -float(x[0])))
     whole_db_tests = list(sorted(whole_db_tests, key=lambda x: -float(x[0])))
 
-    result = 'testy na testsecie (dla porownania z unetem)<br />' + \
-              '<table>' + \
+    result = '<h3>testy na testsecie (dla porownania z unetem)</h3>' + \
+              '<table border=1>' + \
               '<tr><td>avg_dice</td><td>comment</td></tr>'
     for row in test_set_tests:
-        result += '<tr><td>' + row[0] + '</td><td>' + row[1] + '</td></tr>'
-    result += '</table><br />'
-    result += 'testy na całym datasecie z 2022 (prawdopodobnie bardziej wiarygodne wyniki)<br />' + \
-              '<table>' + \
+        result += '<tr><td>{:.4f}</td><td>{}</td><td><a href=/report/{}>raport</a></td></tr>'.format(float(row[0]), row[1], row[2])
+    result += '</table><br /><br />'
+    result += '<h3>testy na całym datasecie z 2022 (prawdopodobnie bardziej wiarygodne wyniki dla podejscia klasycznej analizy obrazow)</h3>' + \
+              '<table border=1>' + \
               '<tr><td>avg_dice</td><td>comment</td></tr>'
     for row in whole_db_tests:
-        result += '<tr><td>' + row[0] + '</td><td>' + row[1] + '</td></tr>'
-        result += '</table><br />'
+        result += '<tr><td>{:.4f}</td><td>{}</td><td><a href=/report/{}>raport</a></td></tr>'.format(float(row[0]), row[1], row[2])
+    result += '</table><br />'
 
     result += '<br /><a href=/test>Dodaj wynik</a>'
     return result
@@ -83,9 +84,17 @@ def test():
                 '<input type="checkbox" name="only_testset" value="testset" />tylko test set (dla uneta)'
                 '(zaznacz jesli testy maja byc na wydzielonym test secie, w przeciwnym razie na calosci 2k obrazkow)<br /><br />'
                 '<input type="text" name="comment" />komentarz (aby latwiej zidentyfikowac wyniki) <br /><br />'
+                '<input type="password" name="password" />haslo (co by jakis bot nie popsul) <br /><br />'
                 f'<input type="submit" value="Testuj"><br /><br />'
                 '</form>')
     elif request.method == 'POST':
+        if request.form['password'] == '':
+            return 'brak hasla'
+
+        expected_passwd_hash = '4a6832b16eec07b46aad62c9f25823e58e8b7abd54404aba8fd43598364b2c98'
+        actual_passwd_hash = sha256(request.form['password'].encode('utf-8')).hexdigest()
+        if actual_passwd_hash != expected_passwd_hash:
+            return 'zle haslo'
 
         if 'only_testset' in request.form:
             only_testset = True
@@ -149,6 +158,20 @@ def test():
                 pass
 
         return f'avg dice: {avg_dice}<br /><a href="/">powrot</a>'
+
+
+@app.route("/report/<id>", methods=['GET'])
+def get_report(id):
+    report_path = os.path.join(eval_online_dir, id, 'eval_report.csv')
+
+    report_html = '<table border=1>'
+    report_html += '<tr><th>ID</th><th>Dice</th></tr>'
+    with open(report_path, 'r') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        for row in csv_reader:
+            report_html += '<tr><td>{}</td><td>{}</td></tr>'.format(row[0], row[1])
+    report_html += '</table>'
+    return report_html
 
 
 if __name__ == "__main__":
